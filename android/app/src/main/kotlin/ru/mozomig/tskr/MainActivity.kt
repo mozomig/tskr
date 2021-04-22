9ac9282756fbc4f61ef5ffced56231f76207b04a
Alexandr Belyakov <mozomig@gmail.com>
2021-04-22 11:19:22 +0300

    init

---
+++ android/app/src/main/kotlin/ru/mozomig/tskr/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.mozomig.tskr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/projects/projects_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter/cupertino.dart';
+
+class ProjectsPage extends StatefulWidget {
+  ProjectsPage({Key? key}) : super(key: key);
+
+  @override
+  _ProjectsPageState createState() {
+    return _ProjectsPageState();
+  }
+}
+
+class _ProjectsPageState extends State<ProjectsPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+    );
+  }
+}
